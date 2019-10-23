09fc68baa6b2b19ca7f7c363cc8fcca32e834961
Florian Léveil <idk@localhost.localdomain>
2019-10-23 11:14:29 +0200

    Leveil Florian B2B

--- tp1/1/index.js
+++ tp1/1/index.js
@@ -7,7 +7,35 @@ exports = module.exports = {
   datasets,
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
+        let placedepart = input.shift()
+        
+        const l = input.length
+        for (let i = 0; i < l ;i++) {
+            let var1 = input[i].split('')
+            
+            placedepart = (parseInt(placedepart)) + (parseInt(var1[0]))
+            placedepart = (parseInt(placedepart)) - (parseInt(var1[2]))
+            
+        }
 
+        let sous = 0
+        let abon = 'OK'
+        if (placedepart < 100) {
+            sous += 1000
+        } else if (l === 42) {
+           sous += 100
+        }
+
+        if (placedepart > 10000) {
+            abon = 'KO'
+        }
+        if (abon === 'KO') {
+            return abon
+        } else {
+            return sous
+        }
+        
+        
     // AND HERE
   },
   verify: function (dataset, output) {
--- tp1/2/index.js
+++ tp1/2/index.js
@@ -7,7 +7,37 @@ exports = module.exports = {
   datasets,
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
-
+    
+      const nbcarton = input.shift()
+      const l = input.length
+      let poid = 0
+      let compteur = 0
+      let poidtotal = 0 
+      
+      for (let i = 0; i < l ;i++) {
+          poid = input[i]
+          console.log(poid)
+          poidtotal = poidtotal + poid
+           
+          if (poidtotal > 100) {
+               compteur++
+          }
+ 
+      }
+      if (poidtotal <= 100){
+          compteur = 1
+          return compteur
+      } else {
+          if (poidtotal === 200) {
+              return compteur
+          } else {
+              compteur = compteur + 1
+              return compteur
+          }
+      }
+      console.log(poidtotal)
+      console.log(compteur)
+      
     // AND HERE
   },
   verify: function (dataset, output) {
